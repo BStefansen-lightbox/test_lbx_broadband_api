b3c2cb235372f5cc304302399581e91efb5e4528
BStefansen-lightbox <bstefansen@lightboxre.com>
2023-05-24 15:14:39 -0400

    first commit

---
+++ lbx_broadband_api/lbx_broadband_api/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for lbx_broadband_api project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lbx_broadband_api.settings')
+
+application = get_asgi_application()
---
+++ lbx_broadband_api/polls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class Foo(models.Model):
+    foo = models.IntegerField(default=0)
+
+class Bar(models.Model):
+    bar = models.IntegerField(default=0)
+    fruit_salad = models.IntegerField(default=0)
+

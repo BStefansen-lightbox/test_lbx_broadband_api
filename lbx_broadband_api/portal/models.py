39e15e806e588825a75264ece2f28d2deedc1c15
BStefansen-lightbox <bstefansen@lightboxre.com>
2023-05-24 16:46:12 -0400

    revamp submission endpoint

--- lbx_broadband_api/portal/models.py
+++ lbx_broadband_api/portal/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+import uuid
+
+
 
 
 class Submissions(models.Model):
+    file_name = models.CharField(max_length=200, default="")
     file_type = models.CharField(max_length=200)
+    backend_qa_results = models.JSONField(default=dict)
+    guid = models.UUIDField(default=uuid.uuid4())
+    isp_name = models.CharField(max_length=200, default="")
+
 
--- lbx_broadband_api/portal/views.py
+++ lbx_broadband_api/portal/views.py
@@ -9,7 +9,7 @@ def index(request):
 
 def submissions(request):
     latest_submission_list = Submissions.objects.order_by("file_type")[:5]
-    output = "<br><br>".join([f"{s.file_type}" for s in latest_submission_list])
+    output = "<br><br>".join([f"{s.__dict__}" for s in latest_submission_list])
     response = HttpResponse(output)
     response.headers["foobar"] = 120
     return response
